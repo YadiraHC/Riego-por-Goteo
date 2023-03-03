a8f2ec6ae6b28c6871f7ce72eae85252d292d8e8
javeireal <alexandercantun744@gmail.com>
2023-03-03 08:30:08 -0500

    cambios x

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  {
+  },
+  {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
